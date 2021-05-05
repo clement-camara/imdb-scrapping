0cc2e8e335b653323cd21f8c813ba70fc128ff79
Florian Kogon <F.Kogon@gmail.com>
2021-05-05 11:54:12 +0200

    add multiple directors method

--- scrapp_refacto.py
+++ scrapp_refacto.py
@@ -13,6 +13,7 @@ pages = [page1, page2, page3, page4, page5]
 headers = ['Titre', 'Année', 'Score', 'Réalisateur', 'Votes', 'Recette']
 movies = pd.DataFrame(columns=headers)
 
+real_directors = {}
 
 def add_title():
     title = film.h3.a.text
@@ -29,11 +30,6 @@ def add_rate():
     row.append(rate)
 
 
-def add_director():
-    director = film.find_all('p')[2].a.text
-    row.append(director)
-
-
 def add_vote_and_recette():
     for i in range(5):
         if i % 5 == 1:
@@ -48,6 +44,20 @@ def add_vote_and_recette():
         else:
             pass
 
+def add_directors(title):
+    directors = ""
+    directors += str(film.find_all('p')[2])
+    splitted_directors = directors.split("Stars:")
+    last_split = splitted_directors[0].replace('<span class="ghost">|</span> ', "").replace('<p class="">', '').replace('Director:', '').replace('Directors:', '').replace('\n', '')
+    end_split = last_split.split('">')
+    if len(end_split) > 3:
+        real_directors[title] = [end_split[3].replace("</a>", ""), end_split[2].split('</a')[0], end_split[1].split('</a')[0]]
+    elif len(end_split) > 2:
+        real_directors[title] = [end_split[2].replace("</a>", ""), end_split[1].split('</a')[0]]
+    else:
+        real_directors[title] = end_split[1].replace("</a>", "")
+    row.append(real_directors[title])
+
 
 def scrapp_all():
     global film, row
@@ -56,10 +66,11 @@ def scrapp_all():
         films = soup.find_all("div", {"class": "lister-item-content"})
         for film in films:
             row = []
+            title = film.h3.a.text
             add_title()
             add_year()
             add_rate()
-            add_director()
+            add_directors(title)
             add_vote_and_recette()
             length = len(movies)
             movies.loc[length] = row

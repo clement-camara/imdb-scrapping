9bcfcddfa8d7b2ce0752d7c97bf2ef462b513f31
Smirnova Nina <ms.nina.smirnova@gmail.com>
2021-05-05 14:47:15 +0200

    - add function for scraping urls - add columns ['Genre', 'Durée'] to data frame 'movies'

--- scrapp_refacto.py
+++ scrapp_refacto.py
@@ -1,16 +1,29 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from urllib.request import urlopen as uReq
 
 
-page1 = requests.get("https://www.imdb.com/search/title/?groups=top_250&sort=user_rating")
-page2 = requests.get("https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc&start=51&ref_=adv_nxt")
-page3 = requests.get("https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc&start=101&ref_=adv_nxt")
-page4 = requests.get("https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc&start=151&ref_=adv_nxt")
-page5 = requests.get("https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc&start=201&ref_=adv_nxt")
-pages = [page1, page2, page3, page4, page5]
+main_url = 'https://www.imdb.com'
 
-headers = ['Titre', 'Année', 'Score', 'Réalisateur', 'Votes', 'Recette']
+# function to get page soup from html page
+def get_page_soup(url):    
+    # opening connection
+    uClient = uReq(url)
+    page_html = uClient.read()
+    uClient.close()
+    #html parser
+    return BeautifulSoup(page_html, "html.parser")
+
+# functon to get new url from the html page
+def get_new_url_from_page(page_soup):    
+    url = page_soup.find("div", {"class":"desc"}).find("a", {"class":"lister-page-next next-page"})
+    if url != None:
+        url = url['href']
+        url = main_url + url
+    return url
+
+headers = ['Titre', 'Année', 'Score', 'Réalisateur', 'Votes', 'Recette', 'Genre', 'Durée']
 movies = pd.DataFrame(columns=headers)
 
 real_directors = {}
@@ -24,11 +37,9 @@ def add_year():
     year = film.h3.find("span", {"class": "lister-item-year text-muted unbold"}).text.strip('I ()')
     row.append(year)
 
-
 def add_rate():
     rate = film.find("div", {"class": "ratings-imdb-rating"}).strong.text
     row.append(rate)
-
 
 def add_vote_and_recette():
     for i in range(5):
@@ -58,25 +69,40 @@ def add_directors(title):
         real_directors[title] = end_split[1].replace("</a>", "")
     row.append(real_directors[title])
 
+def add_genre():
+    genre = film.find("span", {"class":"genre"}).text.strip('\n').split(',')
+    row.append(genre)
+
+def add_duree():
+    durée = film.find("span", {"class":"runtime"}).text.strip(' min')
+    row.append(durée)
 
 def scrapp_all():
     global film, row
-    for page in pages:
-        soup = BeautifulSoup(page.content, 'html.parser')
-        films = soup.find_all("div", {"class": "lister-item-content"})
-        for film in films:
-            row = []
-            title = film.h3.a.text
-            add_title()
-            add_year()
-            add_rate()
-            add_directors(title)
-            add_vote_and_recette()
-            length = len(movies)
-            movies.loc[length] = row
+    url = 'https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc&ref_=adv_prv'
+    films = []
+    while url is not None:
+        page = get_page_soup(url)
+        url = get_new_url_from_page(page)
+    
+        films_on_page = page.findAll("div", {"class":"lister-item-content"})
+        films = films + films_on_page
 
+    for film in films:
+        row = []
+        title = film.h3.a.text
+        add_title()
+        add_year()
+        add_rate()
+        add_directors(title)
+        add_vote_and_recette()
+        add_genre()
+        add_duree()
+        length = len(movies)
+        movies.loc[length] = row
+    
 
 scrapp_all()
 
 
-#print(movies)
+print(movies)
